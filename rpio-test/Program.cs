6984dc7df12c8d0a9fe1ae0995b3bc1653b47911
Dane Chambers <cdanechambers@gmail.com>
2019-01-21 13:47:28 -0800

    Checking in current completed work.

---
+++ rpio-test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Unosquare.RaspberryIO;
+using Unosquare.RaspberryIO.Gpio;
+
+namespace rpio_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gpio Interrupts");
+            var pin = Pi.Gpio.Pin04;
+            Console.WriteLine($"Listening on pin {pin.PinNumber}");
+            pin.PinMode = GpioPinDriveMode.Input;
+            pin.RegisterInterruptCallback(EdgeDetection.FallingEdge, ISRCallback);
+            Console.ReadKey();
+        }
+
+        static void ISRCallback()
+        {
+            Console.WriteLine("Pin Activated...");
+        }
+    }
+}
