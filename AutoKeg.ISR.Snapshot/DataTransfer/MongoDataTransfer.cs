88b4421f83a3fc060e03160f421428680f028b6a
Dane Chambers <cdanechambers@gmail.com>
2019-02-27 22:05:45 -0800

    Removing mongo references

--- AutoKeg.ISR.Snapshot/DataTransfer/MongoDataTransfer.cs
+++ AutoKeg.ISR.Snapshot/DataTransfer/MongoDataTransfer.cs
@@ -1,23 +1,28 @@
 using System.Threading;
 using System.Threading.Tasks;
-using MongoDB.Driver;
+// using MongoDB.Driver;
 
 namespace AutoKeg.ISR.Snapshot.DataTransfer
 {
     public class MongoDataTransfer<T> : IDataTransfer<T> where T : new()
     {
-        private IMongoCollection<T> Collection { get; }
+        // private IMongoCollection<T> Collection { get; }
 
-        public MongoDataTransfer(string mongoUrl, string database, string collection)
-        {
-            Collection = new MongoClient(mongoUrl)
-                .GetDatabase(database)
-                .GetCollection<T>(collection);
-        }
+        // public MongoDataTransfer(string mongoUrl, string database, string collection)
+        // {
+        //     Collection = new MongoClient(mongoUrl)
+        //         .GetDatabase(database)
+        //         .GetCollection<T>(collection);
+        // }
 
-        public async Task SaveDataAsync(T data, CancellationToken cancellationToken = default) =>
-            await Collection.InsertOneAsync(data, options: null, cancellationToken);
+        // public async Task SaveDataAsync(T data, CancellationToken cancellationToken = default) =>
+        //     await Collection.InsertOneAsync(data, options: null, cancellationToken);
 
         public void Dispose() { } // do nothing
+
+        public Task SaveDataAsync(T data, CancellationToken cancellationToken = default)
+        {
+            return Task.CompletedTask;
+        }
     }
 }
